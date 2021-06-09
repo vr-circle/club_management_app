f8cbf972743b87b3906709d9008388fab775cb0e
yscurea <yuuki.990510@gmail.com>
2021-06-09 15:45:54 +0900

    update home

--- lib/home/home.dart
+++ lib/home/home.dart
@@ -1,28 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+final isOpenProvider =
+    StateNotifierProvider<StateIsOpen, bool>((ref) => StateIsOpen());
+
+class StateIsOpen extends StateNotifier<bool> {
+  StateIsOpen() : super(false);
+
+  void toggleIsOpen() {
+    this.state = !this.state;
+  }
+}
 
 class HomePage extends HookWidget {
   @override
   Widget build(BuildContext context) {
+    final _isOpen = useProvider(isOpenProvider);
     return Container(
       child: Column(
         children: [
           Column(
             children: [
-              ListTile(
-                title: Text("全体のイベント"),
+              Consumer(
+                builder: (context, watch, child) {
+                  return ListTile(
+                    title: Text("全体のイベント"),
+                    onTap: () {
+                      watch(isOpenProvider.notifier).toggleIsOpen();
+                    },
+                  );
+                },
               ),
-              Card(
-                child: Text("hogehoge"),
-              ),
-              Card(
-                child: Text("hogehoge"),
+              Visibility(
+                visible: _isOpen,
+                child: Card(
+                  child: Text("hogehoge"),
+                ),
               ),
             ],
           ),
           Column(
             children: [
-              ListTile(
+              const ListTile(
                 title: Text("所属サークルのイベント"),
               ),
               Card(
