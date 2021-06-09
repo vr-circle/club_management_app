8817cc5b4a4b31ead6de7638ca0118c64851e61c
yuusuine <yuuki.990510@gmail.com>
2021-06-10 00:51:47 +0900

    add amplify and start creating auth

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,36 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "ap-northeast-1:0f553f89-d19e-4591-a5d3-f8a693555798",
+                            "Region": "ap-northeast-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "ap-northeast-1_trzVUOHq5",
+                        "AppClientId": "6m5l49a96o0icg7n57e2gd3n78",
+                        "AppClientSecret": "1nknk089frtm48u444vr5dcf24o309k6plrq3j94dk3tfp01tnj0",
+                        "Region": "ap-northeast-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH"
+                    }
+                }
+            }
+        }
+    }
+}''';
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/auth_service.dart';
+import 'package:flutter_application_1/sign_up_page.dart';
+import 'package:flutter_application_1/verification_page.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
+import 'login_page.dart';
 import 'home/home.dart';
 import 'search/search.dart';
 import 'todo/todo.dart';
@@ -13,7 +17,7 @@ void main() {
   initializeDateFormatting().then((_) => runApp(ProviderScope(child: MyApp())));
 }
 
-class MyApp extends HookWidget {
+class ClubManagementApp extends HookWidget {
   static const String _title = 'Circle Management App';
   @override
   Widget build(BuildContext context) {
@@ -72,5 +76,76 @@ class MyPages extends HookWidget {
             ],
             currentIndex: _selectedIndex,
             onTap: context.read(pageIndexProvider.notifier).updateIndex));
+  }
+}
+
+// 1
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final _authService = AuthService();
+  @override
+  void initState() {
+    super.initState();
+    _authService.showLogin();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Photo Gallery App',
+      theme: ThemeData(visualDensity: VisualDensity.adaptivePlatformDensity),
+      // 2
+      home: StreamBuilder<AuthState>(
+          // 2
+          stream: _authService.authStateController.stream,
+          builder: (context, snapshot) {
+            // 3
+            if (snapshot.hasData) {
+              return Navigator(
+                pages: [
+                  // 4
+                  // Show Login Page
+                  if (snapshot.data.authFlowStatus == AuthFlowStatus.login)
+                    MaterialPage(
+                      child: LoginPage(
+                        shouldShowSignPage: _authService.showSignUp,
+                        didProvideCredentials:
+                            _authService.loginWithCredentials,
+                      ),
+                    ),
+
+                  // 5
+                  // Show Sign Up Page
+                  if (snapshot.data.authFlowStatus == AuthFlowStatus.signUp)
+                    MaterialPage(
+                        child: SignUpPage(
+                      shouldShowLogin: _authService.showLogin,
+                      didProvideCredentials: _authService.signUpWithCredentials,
+                    )),
+                  if (snapshot.data.authFlowStatus ==
+                      AuthFlowStatus.verification)
+                    MaterialPage(
+                        child: VerificationPage(
+                      didProvideVerificationCode: _authService.verifyCode,
+                    )),
+
+                  if (snapshot.data.authFlowStatus == AuthFlowStatus.session)
+                    MaterialPage(child: ClubManagementApp())
+                ],
+                onPopPage: (route, result) => route.didPop(result),
+              );
+            } else {
+              // 6
+              return Container(
+                alignment: Alignment.center,
+                child: CircularProgressIndicator(),
+              );
+            }
+          }),
+    );
   }
 }
