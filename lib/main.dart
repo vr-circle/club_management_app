76c6a33da4ac8cf3dbbd7c92c9ff71481eb69c43
yuusuine <yuuki.990510@gmail.com>
2021-06-27 02:54:28 +0900

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:html';
+// import 'dart:html';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/store/store_service.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
