8f00c05b9f37c53de1df71dbc8cebb00faf03abf
yuusuine <yuuki.990510@gmail.com>
2021-07-04 00:04:46 +0900

    remove home widget

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,40 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class HomePage extends StatelessWidget {
   static const String route = '/homepage';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Column(
-            children: [
-              Consumer(
-                builder: (context, watch, child) {
-                  return ListTile(
-                    title: Text('all events'),
-                    onTap: () {},
-                  );
-                },
-              ),
-              Card(
-                child: Text("hogehoge"),
-              ),
-            ],
-          ),
-          Column(
-            children: [
-              const ListTile(
-                title: Text('Club to which you belong'),
-              ),
-              Card(
-                child: Text('event sample'),
-              ),
-            ],
-          )
-        ],
-      ),
-    );
+        body: Center(
+      child: Text('this is home'),
+    ));
   }
 }
