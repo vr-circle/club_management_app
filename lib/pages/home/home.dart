713399bb49c8d322921fa14cd7e0e9209876ad99
yuusuine <yuuki.990510@gmail.com>
2021-07-05 00:51:59 +0900

    update home

--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -1,12 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pages/schedule/schedule.dart';
+
+final scheduleListToday = <Schedule>[
+  Schedule(
+      createdBy: '',
+      details: 'hogehoge',
+      end: DateTime.now(),
+      start: DateTime.now(),
+      place: 'hogehogeplace',
+      title: 'fugafugatitle'),
+  Schedule(
+      createdBy: '',
+      details: 'hogehoge',
+      end: DateTime.now(),
+      start: DateTime.now(),
+      place: 'hogehogeplace',
+      title: 'fugafugatitle'),
+  Schedule(
+      createdBy: '',
+      details: 'hogehoge',
+      end: DateTime.now(),
+      start: DateTime.now(),
+      place: 'hogehogeplace',
+      title: 'fugafugatitle'),
+  Schedule(
+      createdBy: '',
+      details: 'hogehoge',
+      end: DateTime.now(),
+      start: DateTime.now(),
+      place: 'hogehogeplace',
+      title: 'fugafugatitle'),
+];
 
 class HomePage extends StatelessWidget {
   static const String route = '/homepage';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: Text('this is home'),
-    ));
+      body: Column(
+        children: [
+          ListTile(
+            title: Text('Schedule in today'),
+          ),
+          Expanded(
+              child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: scheduleListToday.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                        title: Text(scheduleListToday[index].title),
+                        trailing: Text(scheduleListToday[index].details));
+                  }))
+        ],
+      ),
+    );
   }
 }
